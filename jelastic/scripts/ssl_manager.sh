059209d0aa13e51a9534c2845aaeadd37868b07f
Ihor Kolodyuk <ik@jelastic.com>
2016-11-25 16:24:27 +0100

    CustomSSL support is added

---
+++ jelastic/scripts/ssl_manager.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+inherit glassfish-ssl
+
+. /etc/jelastic/environment
+
+_ASADMIN="/opt/repo/versions/${Version}/bin/asadmin";
+GF_KEYSTORE="/opt/glassfish/glassfish/domains/domain1/config/keystore.jks";
+GF_KEYSTORE_ORIG="/opt/glassfish/glassfish/domains/domain1/config/keystore.jks.orig";
+DAS_MAIN_CONFIG="/opt/glassfish/glassfish/domains/domain1/config/domain.xml"
+PASS_FILE="/opt/repo/.gfpass";
+LEGACY_LIB="/usr/lib/jelastic/libs/glassfish-ssl.lib"
+sed -i "s/--target gfcluster//g" $LEGACY_LIB;
+
+
+function _enableSSL(){
+   enableSSL $@
+}
+
+function _disableSSL(){
+   disableSSL $@
+}
