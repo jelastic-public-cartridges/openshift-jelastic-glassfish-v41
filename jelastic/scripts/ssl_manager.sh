8daabea7771bdcf1538337963dcdc02dbc71ba49
ihorman <ihorman@gmail.com>
2016-12-07 13:17:07 +0100

    Update ssl_manager.sh

--- jelastic/scripts/ssl_manager.sh
+++ jelastic/scripts/ssl_manager.sh
@@ -11,7 +11,7 @@ DAS_MAIN_CONFIG="/opt/glassfish/glassfish/domains/domain1/config/domain.xml"
 PASS_FILE="/opt/repo/.gfpass";
 LEGACY_LIB="/usr/lib/jelastic/libs/glassfish-ssl.lib"
 sed -i 's/--target gfcluster//g' $LEGACY_LIB;
-sed -i 's/reloadService/reloadServicegSilent/g' $LEGACY_LIB;
+sed -i 's/reloadService/restartServicegSilent/g' $LEGACY_LIB;
 
 
 function _enableSSL(){
