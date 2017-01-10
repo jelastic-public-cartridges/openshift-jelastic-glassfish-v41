531bb187b7c26fbf8e46a5903143de6bf9a70e0d
Dimka Tsurko <dt@jelastic.com>
2017-01-10 14:05:01 +0000

    Fix reconfigure error

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -4,4 +4,4 @@
 iptables  -t nat -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8743 
 iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -p tcp --dport 8743 -j ACCEPT
-/etc/init.d/iptables save
+/etc/init.d/iptables save  >/dev/null 2>&1
